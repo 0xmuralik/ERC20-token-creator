ad24f14f784558b9b126b22f8d704fcd8615b28a
SpideyPool <krishna192reddy@gmail.com>
2021-04-02 00:10:53 +0530

    migrated all contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,19 @@
 var Token = artifacts.require("Token");
+var TokenSale = artifacts.require("TokenSale");
 
 module.exports = function(deployer){
-    deployer.deploy(Token,"I Don't Count","IDC",8,10000);
 
+    var tokenName="I Don't Count";
+    var tokenSymbol="IDC";
+    var decimals=8;
+    var tokenSupply=10000;
+
+    deployer.deploy(Token,tokenName,tokenSymbol,decimals,tokenSupply).then(function(){
+
+        var tokenPrice=100000000;
+        var tokensForSale=5000;
+        
+        return deployer.deploy(TokenSale,Token.address,tokenPrice,tokensForSale);
+    });
+    
 }
